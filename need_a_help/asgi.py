eabc288a4368f5395fc97a74a5d774bf00cda498
romano125 <romano.polic12@gmail.com>
2019-03-14 13:05:58 +0100

    changes

--- need_a_help/asgi.py
+++ need_a_help/asgi.py
@@ -2,10 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-<<<<<<< HEAD
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
-=======
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","need_a_help.settings")
->>>>>>> e3720368a7c4a31f6e1d2116715c89750821c8ef
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "need_a_help.settings")
 django.setup()
 application = get_default_application()
--- need_a_help/routing.py
+++ need_a_help/routing.py
@@ -6,27 +6,14 @@ from channels.security.websocket import AllowedHostsOriginValidator, OriginValid
 from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
-<<<<<<< HEAD
     # Empty for now (http->django views is added by default)
     'websocket': AllowedHostsOriginValidator(  # wrapa sockete i provjerava da sve sto radi reqest mora biti u allowed hosts u setting.py
         AuthMiddlewareStack(  # zelimo li da useri unutar chata budu unutar socketa
             URLRouter(
                 [
-                    url(r"^chat/messages/(?P<username>[\w.@+-]+)/$", ChatConsumer),
+                    url(r"^chat/messages/<str:username>", ChatConsumer),
                 ]
             )
         )
     )
-=======
-	# Empty for now (http->django views is added by default)
-	'websocket': AllowedHostsOriginValidator(               ##wrapa sockete i provjerava da sve sto radi reqest mora biti u allowed hosts u setting.py
-		AuthMiddlewareStack(							##zelimo li da useri unutar chata budu unutar socketa
-			URLRouter(
-				[
-					url(r"^chat/messages/<str:username>",ChatConsumer),
-				]
-			)
-		)
-	)
->>>>>>> e3720368a7c4a31f6e1d2116715c89750821c8ef
 })
