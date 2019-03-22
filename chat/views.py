ec0d3a6e0250153146cd9f830b3b4c42d2c732dc
romano125 <romano.polic12@gmail.com>
2019-03-22 16:11:40 +0100

    added tips to chat views

--- chat/views.py
+++ chat/views.py
@@ -10,24 +10,41 @@ from django.contrib.auth.models import User
 
 from .forms import ComposeForm
 from .models import Thread, ChatMessage
-
+from user.models import ClientNotifications, RepairmanNotifications
 
 
 class InboxView(LoginRequiredMixin, ListView):
+    # odkomentiraj model = Thread
     template_name = 'chat/inbox.html'
     context_object_name = 'users'
     #paginate_by = 3
 
     def get_queryset(self):
-       # return Thread.objects.by_user(self.request.user).order_by('-timestamp')
-       #return Thread.objects.filter(Q(first=self.request.user)|Q(second=self.request.user)).order_by('-timestamp').distinct()
-       return ChatMessage.objects.filter(Q(thread__first=self.request.user)|Q(thread__second=self.request.user)).order_by('-timestamp').distinct()
-       
+        # return Thread.objects.by_user(self.request.user).order_by('-timestamp') - točno
+        # return Thread.objects.filter(Q(first=self.request.user)|Q(second=self.request.user)).order_by('-timestamp').distinct()
+        return ChatMessage.objects.filter(Q(thread__first=self.request.user) | Q(thread__second=self.request.user)).order_by('-timestamp').distinct()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        thrd = Thread.objects.filter(Q(first=self.request.user)|Q(second=self.request.user))
+
+        us = self.request.user
+        thrd = Thread.objects.filter(Q(first=self.request.user) | Q(second=self.request.user))
+
+        not_r = RepairmanNotifications.objects.filter(repairman=us, remove=False).order_by('-date')
+        not_c = ClientNotifications.objects.filter(client=us, remove=False).order_by('-date')
+        not_rep = RepairmanNotifications.objects.filter(repairman=us, seen=False).count()
+        not_cli = ClientNotifications.objects.filter(client=us, seen=False).count()
+
+        # users =  ChatMessage.objects.by_user(us).order_by('-timestamp').distinct()
+
+        context['not_r'] = not_r
+        context['not_c'] = not_c
+        context['not_rep'] = not_rep
+        context['not_cli'] = not_cli
         context['th'] = thrd
-        return context   
+        # context['users'] = users
+
+        return context
 
 
 class ThreadView(LoginRequiredMixin, FormMixin, DetailView):
@@ -45,7 +62,7 @@ class ThreadView(LoginRequiredMixin, FormMixin, DetailView):
     def get_object(self):
         other_username = self.kwargs.get("username")
         obj, created = Thread.objects.get_or_new(self.request.user, other_username)
-        if obj == None:    #is?
+        if obj == None:  # is?
             raise Http404
         return obj
 
