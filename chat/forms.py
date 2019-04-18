71178e38aa3da45ac2d7c4364607a88867361dbc
romano125 <romano.polic12@gmail.com>
2019-04-18 19:48:15 +0200

    changes in chat style

--- chat/forms.py
+++ chat/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from crispy_forms.helper import FormHelper
 
 
 class ComposeForm(forms.Form):
+
     message = forms.CharField(
-            widget=forms.TextInput(
-                attrs={"class": "form-control"}
-                )
-            )
+        widget=forms.TextInput(
+            attrs={"class": "form-control"}
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ComposeForm, self).__init__(auto_id=True, *args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
